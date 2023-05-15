f4d8c628d426a6b2737967557f6c875a919dfd52
Pedro Gabriel <pedrpg5@gmail.com>
2023-05-15 13:26:00 -0300

    Realizando a detecção de faces

--- server/Controllers/FaceController.ts
+++ server/Controllers/FaceController.ts
@@ -1,8 +1,11 @@
 import { Request , Response } from 'express';
+import faceDetection from '../FaceAPI/faceDetection';
 
 class FaceController {
     public static async compare(request:Request) {
-        console.log(request)
+        // console.log(request)
+
+        await faceDetection(request.file);
         
     }
 }
