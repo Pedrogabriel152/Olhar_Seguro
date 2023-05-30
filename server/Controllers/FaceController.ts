66e759019903e74595e5e037f19c6b7cc37f9d5e
Pedro Gabriel <pedrpg5@gmail.com>
2023-05-30 15:59:15 -0300

    Atualizando

--- server/Controllers/FaceController.ts
+++ server/Controllers/FaceController.ts
@@ -4,7 +4,7 @@ import faceDetection from '../FaceAPI/faceDetection';
 class FaceController {
     public static async compare(request:Request, response: Response) {
         const {detection} = request.body
-        const result =  await faceDetection(detection);
+        const result: string =  await faceDetection(detection);
 
         if(result === 'unknown'){
             return response.status(300).json({message: "Acesso negado"});
